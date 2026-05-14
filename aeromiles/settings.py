728d9f51389e6de8c9f1e7bb309e69649ef8694d
saikhahummu <saikhahanja@gmail.com>
2026-05-14 12:57:42 +0700

    feat: add deployment stuff

--- aeromiles/settings.py
+++ aeromiles/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,14 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4=imyga6+$@&%mc$ipui@v+a*pvl3@*$@i@9f(1m3b6kro+2p4'
+# Ambil secret key dari environment, atau gunakan default untuk lokal
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-4=imyga6+$@&%mc$ipui@v+a*pvl3@*$@i@9f(1m3b6kro+2p4')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Jika sedang di Render, matikan DEBUG
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
@@ -43,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,3 +131,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+# Tambahkan baris ini
+STATIC_ROOT = BASE_DIR / 'staticfiles'
